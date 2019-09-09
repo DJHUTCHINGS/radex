4a1f51f46356a183fc8e5687909396aedd3b556a
“David <davejh76@gmail.com>
2019-09-09 12:12:40 -0400

    set up templates for components

---
+++ src/output-comments-box/output-comments-box.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+// import './XXX.css';
+
+// import OTHERCOMPONENTS from "../FOLDER/JSFILE";
+
+
+class CLASSNAME extends Component {
+    constructor(){
+        super()
+        this.state = {
+        //   PROPERTY:'',
+        //   PROP2:'DEFAULT'
+        }
+      }
+
+    // METHODNAME = event => {
+    // event.preventDefault()
+
+    // .then( response => console.log(response))
+    // }
+    
+
+
+    render() {
+      return (
+        <div className="">
+
+        </div>
+      );
+    }
+  }
+  
+  export default CLASSNAME;
