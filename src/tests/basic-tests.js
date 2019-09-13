c14b5028ebaa9fb147fac0e2de1fb0a8ee86bff4
“David <davejh76@gmail.com>
2019-09-12 23:08:17 -0400

    work on tests

--- src/tests/basic-tests.js
+++ src/tests/basic-tests.js
@@ -17,31 +17,55 @@ class BasicTests extends Component {
       }
 
 basicTest(strippedInputArray) {
-  console.log("now we are running hte basic test")
+  // console.log("now we are running hte basic test")
   var score = this.initialScore(strippedInputArray)
   if (score.length > 0) {
-    console.log("now we run first tests")
+    // console.log("now we run first tests")
     for (let i = 0; i < strippedInputArray.length; i++) {
-      console.log("now testing the index " + i + ", which is " + strippedInputArray[i])
-      if (score[i][0] === "ص") {score[i][1] = score[i][1] * 2; score[i][2] = ["ص comment"]}
-      if (score[i][0] === "ق") {score[i][1] = score[i][1] * 2; score[i][2] = ["ق comment"]}
-      if (score[i][0] === "ا") {score[i][1] = score[i][1] / 2; score[i][2] = ["ا comment"]}
-      if (score[i][0] === "ج") {score[i][1] = score[i][1] * 2; score[i][2] = ["ج comment"]}
-      if (score[i][0] === "ض") {score[i][1] = score[i][1] * 2; score[i][2] = ["ض comment"]}
-      if (score[i][0] === "م") {score[i][1] = score[i][1] / 2; score[i][2] = ["م comment"]}
-      if (score[i][0] === "ع") {score[i][1] = score[i][1] * 2; score[i][2] = ["ع comment"]}
-      if (score[i][0] === "ح") {score[i][1] = score[i][1] * 2; score[i][2] = ["ح comment"]}
+      // console.log("now testing the index " + i + ", which is " + strippedInputArray[i])
+	      if (score[i][0] ===	"ا"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = ["An aliph is used for numerous grammatical reasons and usually should not be assumed to be part of the root unless all other options have been ruled out."	]};																													
+	      if (score[i][0] ===	"ب"	) {score[i][1] = score[i][1] 	*	1.5	; score[i][2]  = ["If a ب is at the start of the term, it is probably not part of the root; otherwise it probably is."	]};																													
+	      if (score[i][0] ===	"ج"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = ["A ج is always part of the root."	]};																													
+	      if (score[i][0] ===	"د"	) {score[i][1] = score[i][1] 	*	1.75	; score[i][2]  = [	"A د is almost always part of the UNLESS it is substituting for a ت in some rare Form 8 words, in which case it would follow the first radical."	]};																													
+	      if (score[i][0] ===	"ه"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = [	"The ه ـهـ is often found near the end of a word as part of a pronoun; if it is near the front, it is probably part of the root."	]};																													
+	      if (score[i][0] ===	"و"	) {score[i][1] = score[i][1] 	/ 	1.75	; score[i][2]  = [	"The و serves a lot of grammatical functions, but is often part of a root as well, where is sometimes is switched out for an aliph in some forms."	]};																													
+	      if (score[i][0] ===	"ز"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"A ز is almost always part of a root."	]};																													
+	      if (score[i][0] ===	"ح"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"A ح is always part of a root."	]};																													
+	      if (score[i][0] ===	"ط"	) {score[i][1] = score[i][1] 	*	1.75	; score[i][2]  = [	"A ط is almost always of a root, except in certain rare occasions where it substitutes for a ت in Form 8 words, in which case it would follow the first radical."	]};																													
+	      if (score[i][0] ===	"ي"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = [	"A ي is used for many grammatical functions, but can sometimes be part of a root, where it may switch out with a ا in various forms of the word."	]};																													
+	      if (score[i][0] ===	"ك"	) {score[i][1] = score[i][1] 	/ 	1.5	; score[i][2]  = [	"A ك at the beginning or near the end of a term is probably serving a grammatical purpose."	]};																													
+	      if (score[i][0] ===	"ل"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = [	"The ل near the beginning of a term often serves a grammatical purpose."	]};																													
+	      if (score[i][0] ===	"م"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = [	"An م near the start of a word can indicate a meem word, which may be an active or passive participle or noun of place, or noun of instrument or something else; a meem near the end of a word may be part of a pronoun."	]};																													
+	      if (score[i][0] ===	"ن"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = [	"A ن at the very beginning or end of a term may indicate some kind of verb conjugation; if it is not the first letter, but near the start of the term, it may indicate a Form 7 word."	]};																													
+	      if (score[i][0] ===	"س"	) {score[i][1] = score[i][1] 	/ 	2	; score[i][2]  = [	"A س at the very start of a term may indicate the future tense. If it is not the first letter, but is followed by a ت then it may indicate a Form 10 word."	]};																													
+	      if (score[i][0] ===	"ع"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"An ع is almost always part of the root, but note that it is used in several prepositions."	]};																													
+	      if (score[i][0] ===	"ف"	) {score[i][1] = score[i][1] 	*	1.25	; score[i][2]  = [	"A ف at the very start of a term may be a kind of preposition. Otherwise it is probably part of the root."	]};																													
+	      if (score[i][0] ===	"ص"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The ص is essentially always part fo the root."	]};																													
+	      if (score[i][0] ===	"ق"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The ق is always part of the root"	]};																													
+	      if (score[i][0] ===	"ر"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The ر is always part of the root."	]};																													
+	      if (score[i][0] ===	"ش"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The ش is always part of the root."	]};																													
+	      if (score[i][0] ===	"ت"	) {score[i][1] = score[i][1] 	/ 	2.5	; score[i][2]  = [	"The ت is used for so many grammatical purposes that it is almost never part of the root."	]};																													
+	      if (score[i][0] ===	"غ"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The غ is always part of the root."	]};																													
+	      if (score[i][0] ===	"ظ"	) {score[i][1] = score[i][1] 	*	1.75	; score[i][2]  = [	"The ظ is almost always part of the root, expect when it is sometimes substituted for the ت in some Form 8 words, where it will follow the first radical."	]};																													
+	      if (score[i][0] ===	"ض"	) {score[i][1] = score[i][1] 	*	1.75	; score[i][2]  = [	"The ض is almost always part of the root."	]};																													
+	      if (score[i][0] ===	"ذ"	) {score[i][1] = score[i][1] 	*	1.75	; score[i][2]  = [	"The ذ is essentially always part of the root."	]};																													
+	      if (score[i][0] ===	"خ"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The خ is always part of the root."	]};																													
+	      if (score[i][0] ===	"ث"	) {score[i][1] = score[i][1] 	*	2	; score[i][2]  = [	"The ث is always part of the root."	]};																													
+	      if (score[i][0] ===	"ء"	) {score[i][1] = score[i][1] 	/ 	1.5	; score[i][2]  = [	"The ء is a tricky letter that takes many different forms. It it is in this form and at the  end of it term, it may represent a kind of plural and not be part of the root."	]};																													
+	      if (score[i][0] ===	"ى"	) {score[i][1] = score[i][1] 	/ 	1.25	; score[i][2]  = [	"The aliph maksurah often indicates that it or a regular aliph is part of the root."	]};																													
+	      if (score[i][0] ===	"ؤ"	) {score[i][1] = score[i][1] 	*	1.50	; score[i][2]  = [	"A waaw with a hamzah seated on it often indicates that a hamzah or hamzah on an aliph is in the root, but it has been modified for grammatical reasons."	]};																													
+	      if (score[i][0] ===	"ئ"	) {score[i][1] = score[i][1] 	*	1.50	; score[i][2]  = [	"A seated hamzah or hamzah on an an aliph maksurah often indicates that a hamzah of some type is part of the root."	]};																																																								
 
     }
 
 
 
   } else {
-    console.log("there is nothing to score")
+    // console.log("there is nothing to score")
   }
 
-console.log("the final score array is:")
-console.log(score)
+// console.log("the final score array is:")
+// console.log(score)
 return score
   //end of basic text
 }
@@ -49,8 +73,8 @@ return score
 
 
 initialScore(strippedInputArray) {
-console.log("creating intitial score out of the following array")
-console.log(strippedInputArray)
+// console.log("creating intitial score out of the following array")
+// console.log(strippedInputArray)
 let score = []
 
 if (strippedInputArray !== undefined) {
@@ -58,7 +82,7 @@ if (strippedInputArray !== undefined) {
   //initial score is set as a percentage based on how many letters there are
   var defaultScore = 100 / strippedInputArray.length
   if (strippedInputArray.length > 0) {
-    console.log("stripped input length is " + strippedInputArray.length)
+    // console.log("stripped input length is " + strippedInputArray.length)
     for (let i = 0; i < strippedInputArray.length; i++) {
       let thisCharacter = strippedInputArray[i]
       let emptyCommentArray = []
@@ -67,22 +91,18 @@ if (strippedInputArray !== undefined) {
     }
 
   } else {
-    console.log("stripped input is zero")
+    // console.log("stripped input is zero")
   }
 
 } else {
-  console.log("there is no valid input to assess")
+  // console.log("there is no valid input to assess")
 }
 
-console.log("The initial score array is:")
-console.log(score)
+// console.log("The initial score array is:")
+// console.log(score)
 return score;
 }
-
-
-
-
-
+``
 
 
 
@@ -132,6 +152,7 @@ return score;
         
         var newInputObj = {
           rawInput: rawInput,
+          strippedInput: strippedInput,
           strippedInputArray: strippedInputArray,
           score: basicTestScore,
         }
--- src/tests/main-tests.js
+++ src/tests/main-tests.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 
-
+ 
 import OutputCharsBox from "../output-chars-box/output-chars-box";
 import OutputCommentsBox from "../output-comments-box/output-comments-box";
 import ScoreDisplay from "../score-display/score-display";
@@ -13,16 +13,85 @@ class MainTests extends Component {
         this.state = {
 
         }
+        this.runMainTests = this.runMainTests.bind(this);
       }
 
- 
+      runMainTests(strippedInput, score) {
+      console.log("now starting main tests...")
+      let mainTestResults = {
+        score: score,
+        meemWord: false,
+        ALword: false,
+        verbalNoun: false,
+        testsRan: [],
+        mainTestOutputComments: []
+      }
+      let inputLength = mainTestResults.score.length
+      //add extra spaces to word for tests:
+      
+      if (inputLength > 0) {
+        var pos1 = mainTestResults.score[0][0]
+        var pos1scr = mainTestResults.score[0][1]
+        if (inputLength > 1) {
+          var pos2 = score[1][0]
+          var pos2scr = mainTestResults.score[1][1]
+          if (inputLength > 2) {
+            var pos3 = score[2][0]
+            var pos3scr = mainTestResults.score[2][1]
+            if (inputLength > 3) {
+              var pos4 = score[3][0]
+              var pos4scr = mainTestResults.score[3][1]
+              //!!!!!!!!!!!!! NEED TO FINISH
+            }
+          }
+        }
+      }
+      console.log(pos1, pos1scr, pos2, pos2scr, pos3, pos3scr, pos4, pos4scr)
+      const diff = 100/inputLength;
+      
+      mainTestResults.testsRan.push("initial م");
+      if (inputLength > 3 && pos1 === "م") {mainTestResults.score[0][1] = mainTestResults.score[0][1] / diff;
+      mainTestResults.mainTestOutputComments.push("If there is an initial meem, it is probably not part of the root.");
+      }
+
+      mainTestResults.testsRan.push("initial ا");
+      if (inputLength > 4 && pos1 === "ا") {mainTestResults.score[0][1] = mainTestResults.score[0][1] / diff;
+      mainTestResults.mainTestOutputComments.push("Aliphs are usually not part of the root");
+      }
+
+      mainTestResults.testsRan.push("initial الـ");
+      if (inputLength > 4 && pos1 === "ا" && pos2 === "ل") {
+        mainTestResults.score[0][1] = mainTestResults.score[0][1] / diff;
+        mainTestResults.score[1][1] = mainTestResults.score[1][1] / diff;
+        mainTestResults.mainTestOutputComments.push("ال are usually not part of the root");
+        mainTestResults.ALword = true;
+      }
+
+      mainTestResults.testsRan.push("final ـها");
+      if (inputLength > 5 && score[inputLength-1][0] === "ا" && score[inputLength-2][0] === "ه" ) {
+        mainTestResults.score[inputLength-1][1] = mainTestResults.score[inputLength-2][1] / diff;
+        mainTestResults.score[inputLength-2][1] = mainTestResults.score[inputLength-2][1] / diff;
+        mainTestResults.mainTestOutputComments.push("ِA ها at the end of the word is usually a pronoun");
+      }
+
+      return mainTestResults
+      }
+
+
+
     render(props) {
         console.log("At Main Tests now")
         //the following needs to be updated
         // console.log(this.props.rawInput)
         const { newInputObj } = this.props;
-        const {score, rawInput} = newInputObj
+        const {score, rawInput, strippedInput} = newInputObj
         console.log(score)
+
+var mainTestResults = this.runMainTests(strippedInput, score);
+console.log("the main test results handed back are:")
+console.log(mainTestResults)
+
+
 
       return (
         <div className="">
